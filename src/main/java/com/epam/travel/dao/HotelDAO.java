0834ae1f99103e354768ad2c651a4f76eccec0ac
brainydamage <brainydamage@gmail.com>
2017-10-05 00:56:54 +0300

    add DAO for hotel

---
+++ src/main/java/com/epam/travel/dao/HotelDAO.java
@@ -0,0 +1,10 @@
+package com.epam.travel.dao;
+
+import com.epam.travel.model.Hotel;
+import java.util.List;
+
+public interface HotelDAO {
+    List<Hotel> getAllHotels();
+    void addHotel(Hotel hotel);
+    Hotel findHotel(int id);
+}
