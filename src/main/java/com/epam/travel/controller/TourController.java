7bab9a84a7c6bd66d8ed6e0fadc58969773521d5
Zadorozhnyy Semen <zadorozhnyysemen@gmail.com>
2017-10-05 02:34:13 +0300

    добавлен рилэйшн сервис и имплементация для получения всех рилэйшенов

--- src/main/java/com/epam/travel/controller/TourController.java
+++ src/main/java/com/epam/travel/controller/TourController.java
@@ -1,8 +1,10 @@
 package com.epam.travel.controller;
 
+import com.epam.travel.dao.RelationDAO;
 import com.epam.travel.model.Hotel;
 import com.epam.travel.model.User;
 import com.epam.travel.service.HotelService;
+import com.epam.travel.service.RelationService;
 import com.epam.travel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,9 @@ public class TourController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    RelationService relationService;
 
     @RequestMapping("/")
     public String getIndex() {
